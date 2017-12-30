c81c82c68b850306173d5f64a4f7e05555643f54
Matthew McCallum <matt.c.mccallum@gmail.com>
2017-12-30 14:25:35 -0800

    * Fixed non-standard python formatting to better adhere to coding standards

--- RainbowGram.py
+++ RainbowGram.py
@@ -1,51 +1,52 @@
-'''
+"""
 Created 12-26-17 by Matthew C. McCallum
-'''
+"""
+
 
 def RainbowGram( signal, samp_rate ):
-	'''
-	A quick and dirty function for plotting my personal version of the RainbowGram
-	'''
+    """
+    A quick and dirty function for plotting my personal version of the RainbowGram
+    """
 
-	# Parameters
-	win_len_secs = 0.02
-	win_len = int( win_len_secs*samp_rate )
-	overlap = 1.0 - 1.0/2.0/2.0/2.0
-	frame_inc = int( ( 1 - overlap )*win_len )
-	num_frames = math.floor( ( sig_len - win_len )/frame_inc ) + 1
-	window = np.hamming( win_len )
-	fft_size = int( win_len*2 )
-	saturation = 0.9
-	dyn_range = 60
+    # Parameters
+    win_len_secs = 0.02
+    win_len = int( win_len_secs*samp_rate )
+    overlap = 1.0 - 1.0/2.0/2.0/2.0
+    frame_inc = int( ( 1 - overlap )*win_len )
+    num_frames = math.floor( ( sig_len - win_len )/frame_inc ) + 1
+    window = np.hamming( win_len )
+    fft_size = int( win_len*2 )
+    saturation = 0.9
+    dyn_range = 60
 
-	# Working variables
-	expected_phase = np.arange( fft_size )/fft_size*2.0*np.pi*frame_inc
+    # Working variables
+    expected_phase = np.arange( fft_size )/fft_size*2.0*np.pi*frame_inc
 
-	# Create spectrogram
-	frame_indices = np.arange( num_frames, dtype='int32' )
-	freq_indices = np.arange( win_len, dtype='int32' )
-	spec_indices = np.add( *np.meshgrid( frame_indices*frame_inc, freq_indices ) )
-	spec = np.fft.fft( np.dot( np.diag( window ), signal[spec_indices] ), fft_size, axis=0 )
+    # Create spectrogram
+    frame_indices = np.arange( num_frames, dtype='int32' )
+    freq_indices = np.arange( win_len, dtype='int32' )
+    spec_indices = np.add( *np.meshgrid( frame_indices*frame_inc, freq_indices ) )
+    spec = np.fft.fft( np.dot( np.diag( window ), signal[spec_indices] ), fft_size, axis=0 )
 
-	# Get colors from phase
-	phase_spec = np.angle( spec )
-	phase_diff = ( ( phase_spec[:,1:] - phase_spec[:,:-1] ).T - expected_phase ).T
-	phase_diff = phase_diff/np.pi
-	frame_offset = ( np.around( phase_diff ) - phase_diff )*fft_size/2.0/frame_inc
+    # Get colors from phase
+    phase_spec = np.angle( spec )
+    phase_diff = ( ( phase_spec[:,1:] - phase_spec[:,:-1] ).T - expected_phase ).T
+    phase_diff = phase_diff/np.pi
+    frame_offset = ( np.around( phase_diff ) - phase_diff )*fft_size/2.0/frame_inc
 
-	# Get image
-	hue = frame_offset/max(np.max(np.max(frame_offset)),abs(np.min(np.min(frame_offset))))
-	hue[hue>1.0] = 1.0
-	hue[hue<-1.0] = -1.0
-	hue = ( hue + 1.0 )/2.0
-	sat = np.ones( hue.shape )*saturation
-	val = 20*np.log10( np.abs( spec[:,1:] ) )
-	val = val - np.max( np.max( val ) )
-	val[val<-dyn_range] = -dyn_range
-	val += dyn_range
-	val /= dyn_range
-	image = [[colorsys.hsv_to_rgb( h, s, v ) for h, s, v in zip( rowh, rows, rowv )] for rowh, rows, rowv in zip( hue, sat, val )]
-	image = np.array( image )
+    # Get image
+    hue = frame_offset/max(np.max(np.max(frame_offset)),abs(np.min(np.min(frame_offset))))
+    hue[hue>1.0] = 1.0
+    hue[hue<-1.0] = -1.0
+    hue = ( hue + 1.0 )/2.0
+    sat = np.ones( hue.shape )*saturation
+    val = 20*np.log10( np.abs( spec[:,1:] ) )
+    val = val - np.max( np.max( val ) )
+    val[val<-dyn_range] = -dyn_range
+    val += dyn_range
+    val /= dyn_range
+    image = [[colorsys.hsv_to_rgb( h, s, v ) for h, s, v in zip( rowh, rows, rowv )] for rowh, rows, rowv in zip( hue, sat, val )]
+    image = np.array( image )
 
-	plt.imshow( image )
-	plt.show()
+    plt.imshow( image )
+    plt.show()
--- SubBinSpecAnalyzer.py
+++ SubBinSpecAnalyzer.py
@@ -1,64 +1,65 @@
-'''
+"""
 Created 12-26-17 by Matthew C. McCallum
-'''
+"""
 
 import numpy as np
 
+
 class SubBinSpecAnalyzer( object ):
-	'''
-	An analyzer for re-estimating magnitude and phase between integer bin indices in
-	a spectrogram.
-	'''
+    """
+    An analyzer for re-estimating magnitude and phase between integer bin indices in
+    a spectrogram.
+    """
 
-	def __init__( self, fft_size, window, resolution_mult ):
-		'''
-		Constructor.
+    def __init__( self, fft_size, window, resolution_mult ):
+        """
+        Constructor.
 
-		Args:
-			fft_size -> int - The FFT size used for analysis of any magnitudes and phases
-			this class wishes to re-estimate.
+        Args:
+            fft_size -> int - The FFT size used for analysis of any magnitudes and phases
+            this class wishes to re-estimate.
 
-			window -> np.ndarray - A 1D array containing the STFT analysis window used for
-			any magnitudes or phases this class wishes to re-estimate.
+            window -> np.ndarray - A 1D array containing the STFT analysis window used for
+            any magnitudes or phases this class wishes to re-estimate.
 
-			resolution_mult -> int - The number of intervals between each STFT bin that this
-			class uses as a lookup table to re-estimate magnitudes and phases. The higher this
-			number is the more accurate it is to a point.
-		'''
-		sub_bin_spec = np.fft.fft( window, fft_size*resolution_mult );
-		self._subbin_resolution = resolution_mult
-		self._subbin_mag = np.abs( sub_bin_spec )
-		self._subbin_mag = np.concatenate( ( self._subbin_mag[:resolution_mult], self._subbin_mag[-resolution_mult:] ) )
-		self._subbin_phase = np.angle( sub_bin_spec )
-		self._subbin_phase = np.concatenate( ( self._subbin_phase[:resolution_mult], self._subbin_phase[-resolution_mult:] ) )
-		self._win_energy = np.sum( window )
+            resolution_mult -> int - The number of intervals between each STFT bin that this
+            class uses as a lookup table to re-estimate magnitudes and phases. The higher this
+            number is the more accurate it is to a point.
+        """
+        sub_bin_spec = np.fft.fft( window, fft_size*resolution_mult );
+        self._subbin_resolution = resolution_mult
+        self._subbin_mag = np.abs( sub_bin_spec )
+        self._subbin_mag = np.concatenate( ( self._subbin_mag[:resolution_mult], self._subbin_mag[-resolution_mult:] ) )
+        self._subbin_phase = np.angle( sub_bin_spec )
+        self._subbin_phase = np.concatenate( ( self._subbin_phase[:resolution_mult], self._subbin_phase[-resolution_mult:] ) )
+        self._win_energy = np.sum( window )
 
-	def GetMag( self, bin_mags, bin_offsets ):
-		'''
-		Re-estimate a set of magnitudes at a given non-integer offset from their original bin indices.
+    def GetMag( self, bin_mags, bin_offsets ):
+        """
+        Re-estimate a set of magnitudes at a given non-integer offset from their original bin indices.
 
-		Args:
-			bin_mags - np.ndarray - An array of the bin magnitudes at integer DFT indices that are representative 
-			of a peak at a potentially non-integer index.
+        Args:
+            bin_mags - np.ndarray - An array of the bin magnitudes at integer DFT indices that are representative
+            of a peak at a potentially non-integer index.
 
-			bin_offsets - np.ndarray - An array of float offsets to re-estimate the magnitude of each of the bins provided
-			in bin_mags at.
-		'''
-		assert all( np.abs( bin_offsets ) < 1.0*( self._subbin_resolution-1 )/self._subbin_resolution ), 'Tried to get subbin accuracy outside of the range of the SubBinSpecAnalyzer.'
-		indices = np.around( bin_offsets*self._subbin_resolution ).astype( 'int32' )
-		return bin_mags/self._subbin_mag[indices]*self._win_energy
+            bin_offsets - np.ndarray - An array of float offsets to re-estimate the magnitude of each of the bins provided
+            in bin_mags at.
+        """
+        assert all( np.abs( bin_offsets ) < 1.0*( self._subbin_resolution-1 )/self._subbin_resolution ), 'Tried to get subbin accuracy outside of the range of the SubBinSpecAnalyzer.'
+        indices = np.around( bin_offsets*self._subbin_resolution ).astype( 'int32' )
+        return bin_mags/self._subbin_mag[indices]*self._win_energy
 
-	def GetPhase( self, bin_phases, bin_offsets ):
-		'''
-		Re-estimate a set of phases at a given non-integer offset from their original bin indices.
+    def GetPhase( self, bin_phases, bin_offsets ):
+        """
+        Re-estimate a set of phases at a given non-integer offset from their original bin indices.
 
-		Args:
-			bin_mags - np.ndarray - An array of the bin phases at integer DFT indices that are representative 
-			of a peak at a potentially non-integer index.
+        Args:
+            bin_mags - np.ndarray - An array of the bin phases at integer DFT indices that are representative
+            of a peak at a potentially non-integer index.
 
-			bin_offsets - np.ndarray - An array of float offsets to re-estimate the phase of each of the bins provided
-			in bin_mags at.
-		'''
-		assert all( np.abs( bin_offsets ) < 1.0*( self._subbin_resolution-1 )/self._subbin_resolution ), 'Tried to get subbin accuracy outside of the range of the SubBinSpecAnalyzer.'
-		indices = np.around( bin_offsets*self._subbin_resolution ).astype( 'int32' )
-		return bin_phases + self._subbin_phase[indices]
+            bin_offsets - np.ndarray - An array of float offsets to re-estimate the phase of each of the bins provided
+            in bin_mags at.
+        """
+        assert all( np.abs( bin_offsets ) < 1.0*( self._subbin_resolution-1 )/self._subbin_resolution ), 'Tried to get subbin accuracy outside of the range of the SubBinSpecAnalyzer.'
+        indices = np.around( bin_offsets*self._subbin_resolution ).astype( 'int32' )
+        return bin_phases + self._subbin_phase[indices]
