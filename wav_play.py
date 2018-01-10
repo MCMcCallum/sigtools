c94fefc0ddf7ed71020fdf7a7ad05b9c9c00f492
Matthew McCallum <matt.c.mccallum@gmail.com>
2018-01-09 20:21:54 -0800

    * Added plotting functionality to the experimental script * Fixed some bugs in finding the phasor to add to each Kalman filter * Reduced the number of phasors being considered for each Kalman filter by only considering a subset of the frequency range, peaks, and large magnitudes.

--- wav_play.py
+++ wav_play.py
@@ -49,7 +49,7 @@ class WavPlay( object ):
         interleaved_array = np.zeros( audio.shape[1] * self._fmt.n_channels )
         for chan in range( self._fmt.n_channels ):
             interleaved_array[chan::self._fmt.n_channels] = audio[chan,:]
-        interleaved_array = interleaved_array*( 2**( fmt.bit_depth-1 ) )
+        interleaved_array = interleaved_array*( 2**( self._fmt.bit_depth-1 ) )
         interleaved_array = interleaved_array.astype( int )
         interleaved_array = interleaved_array.tolist()
         audio_string = struct.pack( self._fmt.PackingString( num_frames ), *interleaved_array )
