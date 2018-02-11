1b0aef69b7f32bf015807f22956f2d6e5eed7327
Matthew McCallum <matt.c.mccallum@gmail.com>
2018-02-10 21:27:45 -0800

    * Removed obsolete tests on non-existent class * Added ability to read wav files in in different formats

--- wav_read.py
+++ wav_read.py
@@ -20,46 +20,57 @@ class WavRead( object ):
     """
     A wave reader object for getting samples from file
     """
+    SAMPLE_FMT_NONE = 'None'
+    SAMPLE_FMT_INT_INTERLEAVED = 'Interleaved Integers'
+    SAMPLE_FMT_FLOAT_ARRAY = 'Float 2D Array'
 
-    def __init__( self, filename ):
+    def __init__( self, filehandle ):
         """
         Constructor.
 
         Args:
-            filename -> str - A string describing the filename and path of the audio wav file to be read.
+            filename -> str or seekable file - A string or seekable file like object describing either the filename and
+            path of the audio wav file to be read, or a stream to the file data itself.
         """
-        self._filename = filename
-        with wave.open(self._filename, 'r') as audio:
+        self._file = filehandle
+        self._data = None
+        self._data_fmt = self.SAMPLE_FMT_NONE
+
+        with wave.open(self._file, 'r') as audio:
             self._num_frames = audio.getnframes()
             self._fmt = WavFmt.FromWav( audio )
 
-    def GetSamplesInterleavedInt( self ):
+    def ReadSamplesInterleavedInt( self ):
         """
-        Get all samples from the wav file as integers in an interleaved list.
+        Reads all samples from the wav file as integers in an interleaved list.
+        This replaces any previous data read from the wav file.
 
         Return:
             list(int) - A list of interleaved samples from the audio file.
         """
-        with wave.open( self._filename, 'r' ) as audio:
+        with wave.open( self._file, 'r' ) as audio:
             data = audio.readframes( self._num_frames )
         data = struct.unpack( self._fmt.PackingString( self._num_frames ), data )
 
-        return list(data)
+        self._data_fmt = self.SAMPLE_FMT_INT_INTERLEAVED
+        self._data = list(data)
 
-    def GetSamplesFloat( self ):
+    def ReadSamplesFloat( self ):
         """
-        Get all samples from the wav file as floats in the range -1.0 <= sample <= 1.0
+        Reads all samples from the wav file as floats in the range -1.0 <= sample <= 1.0.
+        This replaces any previous data read from the wav file.
 
         Return:
             np.ndarray - An array of dimensions (num_channels, num_frames) containing float valued audio samples.
         """
-        data = self.GetSamplesInterleavedInt()
+        self.ReadSamplesInterleavedInt()
 
         return_array = np.zeros( ( self._fmt.n_channels, self._num_frames ) )
         for channel in range( self._fmt.n_channels ):
             return_array[channel,:] = np.array( data[channel::self._fmt.n_channels] )/( 2.0**self._fmt.bit_depth )
 
-        return return_array
+        self._data_fmt = self.SAMPLE_FMT_FLOAT_ARRAY
+        self._data = return_array
 
     @property
     def fmt( self ):
@@ -70,3 +81,23 @@ class WavRead( object ):
             WavFmt - An object containing the audio format parameters.
         """
         return self._fmt
+
+    @property
+    def data( self ):
+        """
+        Any data that has been previously read in form the wave file.
+
+        Return:
+            ? - Audio sample data in the format that was most recently read from file.
+        """
+        return self._data
+
+    @property
+    def data_fmt( self ):
+        """
+        The current audio sample format of the data.
+
+        Return:
+            str - A string constant describing the format in which the class's data is in.
+        """
+        return self._data_fmt
