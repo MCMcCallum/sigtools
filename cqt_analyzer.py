27399e1be32b633868fda0b3e0025eadf17e2f8d
Matt McCallum <mmccallum@gracenote.com>
2018-06-26 13:52:38 -0700

    * Allowed number of windows to be provided in Analyze call to CQTAnalyzer

--- cqt_analyzer.py
+++ cqt_analyzer.py
@@ -40,7 +40,7 @@ class CQTAnalyzer(object):
         self._samples_per_octave = samples_per_octave
         self._num_windows = num_windows
 
-    def Analyze(self, audio_sig, start_idx, samp_rate):
+    def Analyze(self, audio_sig, start_idx, samp_rate, num_windows=None):
         """
         This function will perform a CQT analysis of signal starting at the index requested. The analysis will not
         go to the end of the signal, but output a fixed number of analysis windows following the requested index.
@@ -53,19 +53,26 @@ class CQTAnalyzer(object):
             windows are centered at audiosig[idx*self._hop].
 
             samp_rate: float - Sampling rate in Hz of the audio data that will be provided to the analyzer.
+
+            num_windows: int - The number of windows to analyze from the provided starting index with the configured
+            hop number of samples in between each.
         """
+        # If no number of windows is provided, use default
+        if not num_windows:
+            num_windows = self._num_windows
+
         hop_samples = self._hop*samp_rate
         hop_samples = int(2**5 * round(hop_samples/(2**5)))
         # TODO [matthew.mccallum 06.15.18]: Here I add on 1.0 seconds to catch the last window, and chop off any excess later. 
         # This is a little sloppy, but there is only so much thyme. I should really caclulate the additional audio required.
-        audio_sig = audio_sig[int(start_idx*hop_samples):int(start_idx*hop_samples + hop_samples*self._num_windows + samp_rate)] 
+        audio_sig = audio_sig[int(start_idx*hop_samples):int(start_idx*hop_samples + hop_samples*num_windows + samp_rate)] 
         return np.abs(librosa.core.cqt(audio_sig, 
                                 samp_rate, 
                                 hop_samples, 
                                 self._min_freq, 
                                 self._octaves*self._samples_per_octave, 
                                 self._samples_per_octave, 
-                                tuning=0.0)[:,:self._num_windows])
+                                tuning=0.0)[:,:num_windows])
 
     @property
     def window_rate(self):
