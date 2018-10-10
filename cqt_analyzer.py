63c2f7510fc88255502b0c2b4eeb6cc8cfd09758
Matt McCallum <matt.c.mccallum@gmail.com>
2018-10-10 11:47:47 -0700

    * Corrected bug where an incorrect windowing rate could be returned * NOTE: This changes the CQTAnalyzer interface, be sure to update your code before updating this submodule

--- cqt_analyzer.py
+++ cqt_analyzer.py
@@ -19,7 +19,7 @@ class CQTAnalyzer(object):
     settings in the object across calls to Analyze(...).
     """
 
-    def __init__(self, samples_per_octave, octaves, min_freq, hop, num_windows):
+    def __init__(self, samples_per_octave, octaves, min_freq, hop, num_windows, samp_rate):
         """
         Constructor.
 
@@ -33,14 +33,18 @@ class CQTAnalyzer(object):
             hop: float - The number of seconds to skip between successive CQT analysis windows.
             
             num_windows: int - The number of CQT windows to analyze following the requested analysis point.
+
+            samp_rate: int - The sampling rate in Hz to be used for analysis - note this will affect the hop size and
+            should be reconfigured for each audio sample if necessary.
         """
         self._hop = hop
         self._min_freq = min_freq
         self._octaves = octaves
         self._samples_per_octave = samples_per_octave
         self._num_windows = num_windows
+        self._samp_rate = samp_rate
 
-    def Analyze(self, audio_sig, start_idx, samp_rate, num_windows=None):
+    def Analyze(self, audio_sig, start_idx, num_windows=None):
         """
         This function will perform a CQT analysis of signal starting at the index requested. The analysis will not
         go to the end of the signal, but output a fixed number of analysis windows following the requested index.
@@ -61,12 +65,9 @@ class CQTAnalyzer(object):
         if not num_windows:
             num_windows = self._num_windows
 
-        hop_samples = self._hop*samp_rate
-        hop_samples = int(2**(self._octaves - 1) * round(hop_samples/(2**(self._octaves - 1))))
-
         # TODO [matthew.mccallum 06.15.18]: Here I add on 1.0 seconds to catch the last window, and chop off any excess later. 
         # This is a little sloppy, but there is only so much thyme. I should really caclulate the additional audio required.
-        audio_sig = audio_sig[int(start_idx*hop_samples):int(start_idx*hop_samples + hop_samples*num_windows + samp_rate)] 
+        audio_sig = audio_sig[int(start_idx*self.hop):int(start_idx*self.hop + self.hop*num_windows + samp_rate)] 
 
         # TODO [matt.c.mccallum 08.21.18]: Here we make sure the number of samples is not close to a prime number to avoid problems
         # resampling with scipy.
@@ -75,7 +76,7 @@ class CQTAnalyzer(object):
 
         return np.abs(cqt(audio_sig, 
                         samp_rate, 
-                        hop_samples, 
+                        self.hop, 
                         self._min_freq, 
                         self._octaves*self._samples_per_octave, 
                         self._samples_per_octave, 
@@ -88,7 +89,31 @@ class CQTAnalyzer(object):
 
         Returns the effective windowing rate of the CQT anaylsis in Hz.
         """
-        return 1.0/self._hop
+        return 1.0/self.hop
+
+    @property
+    def hop(self):
+        """
+        Type: int
+
+        The number of samples by which to hop by between each CQT analysis window.
+        """
+        hop_samples = self._hop*self._samp_rate
+        hop_samples = int(2**(self._octaves - 1) * round(hop_samples/(2**(self._octaves - 1))))
+        return hop_samples
+
+    @property
+    def samp_rate(self):
+        """
+        Type: int
+
+        The sampling rate that the provided samples will be considered to be at upon analysis.
+        In Hz.
+        """
+        return self._samp_rate
+    @samp_rate.setter
+    def samp_rate(self, samp_rate):
+        self._samp_rate = samp_rate
 
     @property
     def num_windows(self):
